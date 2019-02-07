2c8b862764732b83c8f8d36484ac07cd2f1843ba
Oscar Frederick Perez Reyes <a01271794@itesm.mx>
2019-02-07 18:15:48 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,13 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
+  def position_taken?(board, index)
+    if board[index] == "" || board[index] == " "
+      return false
+    else
+      return true
+    end
+  end
+  
   if board[index] == "" || board[index] == " " || board[index] == "O" || board[index] == "X" && position_taken == false
     return true
   else
@@ -8,10 +16,4 @@ def valid_move?(board, index)
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
-def position_taken?(board, index)
-  if board[index] == "" || board[index] == " "
-    return false
-  else
-    return true
-  end
-end
+
