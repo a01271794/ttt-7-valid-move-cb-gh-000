1981dec56a681b12dbd067b53381b97197de7775
Oscar Frederick Perez Reyes <a01271794@itesm.mx>
2019-02-07 18:26:43 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,23 +1,4 @@
 def valid_move?(board, index)
-  def position_taken?(board, index)
-    if board[index] == " " || board[index] == "" || board[index] == nil
-      return false
-    else
-      return true
-    end
-  end
-
-  def on_board?(index)
-    if num.between?(0, 8) == true
-      return true
-    else
-      return false
-    end
-  end
-
-  if on_board == true && position_taken == false
+  if board[index]=="" || board[index]==" "
     return true
-  else
-    return false
-  end
 end
